6fcc1941a823aff04fde29696c21f7f489c6609a
Karol <kmaksymiuk5555@gmail.com>
2021-05-25 13:50:52 +0200

    2.0 ended

--- app/src/main/java/pollub/ism/lab10/MainActivity.java
+++ app/src/main/java/pollub/ism/lab10/MainActivity.java
@@ -3,12 +3,41 @@ package pollub.ism.lab10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import pollub.ism.lab10.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding okno = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        okno = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(okno.getRoot());
+
+        okno.pasekPostepu.setProgress(0);
+        okno.pasekPostepu.setMax(100);
+        okno.przycisk3.setEnabled(true);
+        okno.przycisk4.setEnabled(false);
+    }
+
+
+    public void przycisk1(View view) {
+
+    }
+
+    public void przycisk2(View view) {
+
+    }
+
+    public void przycisk3(View view) {
+
+    }
+
+    public void przycisk4(View view) {
+
     }
 }
